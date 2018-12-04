6b7c63a1382ea92b579e2d0a39753a4295d3c6db
Phil Crissman <phcrissm@microsoft.com>
2018-12-03 16:42:00 -0800

    Day 3

---
+++ 3.rb
@@ -0,0 +1,36 @@
+input = File.read("./input3.txt").split("\n")
+
+# returns [id,left,top,x,y]
+def parse(line)
+  result = line.match(/\#(\d{1,4})\s\@\s(\d{1,4}),(\d{1,4})\:\s(\d{1,4})x(\d{1,4})$/)
+  result.captures.map(&:to_i)
+end
+
+class Claim
+  attr_reader :id, :left, :top, :x, :y
+
+  def initialize(id, left, top, x, y)
+    @id, @left, @top, @x, @y = id, left, top, x, y
+  end
+end
+
+@claims = input.map{|line| Claim.new(*parse(line))}
+
+@fabric = (1..1000).map{|n| (1..1000).map{[]}}
+
+@claims.each do |claim|
+  claim.y.times do |yindex|
+    claim.x.times do |xindex|
+      @fabric[claim.left+xindex][claim.top+yindex] << claim.id
+    end
+  end
+end
+
+count = @fabric.map{|row| row.select{|square| square.count > 1}.count}.inject(:+)
+puts "Part 1: #{count}"
+
+require 'set'
+claim_ids = Set.new(@fabric.map{|row| row.select{|square| square.count > 1}}.flatten.uniq)
+all_ids = Set.new((1..@claims.count))
+
+puts "Part 2: #{(all_ids - claim_ids).to_a}"
